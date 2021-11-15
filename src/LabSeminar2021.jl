d788b0a4df7719c37d148d25a7a1716f26b15522
Yosuke Isoda <y.isoda.3953@gmail.com>
2021-11-15 23:25:27 +0900

    Fixed

--- src/LabSeminar2021.jl
+++ src/LabSeminar2021.jl
@@ -5,8 +5,9 @@ function PlotReflectivity()
     ε_0, e, ħ = 8.85418782e-12, 1.60217662e-19, 1.05457e-34
     ε(ω, ω_p) = 1 - ω_p^2 / ω^2
 
+    σ = 3.6e7 # Ω⋅m
 
-    ε_damped(ω) = 1 - (3.6e-5) / (im * ω * ε_0) #8.85418782e-12)
+    ε_damped(ω) = 1 - (σ) / (im * ω * ε_0) #8.85418782e-12)
 
     ε_actual(ω, ω_p, σ) = 1 - ω_p^2 / (ω^2 + im * ω * ε_0 * ω_p^2 / σ)
 
@@ -15,16 +16,15 @@ function PlotReflectivity()
     x = range(0.1, stop = 2, step = 0.01)
 
     ħωp_eV = 15.2
-    omega_p = 0.005 * ħωp_eV * e / ħ
+    omega_p = ħωp_eV * e / ħ
 
 
     ω_range_max = omega_p * 2
-    x2 = range(omega_p / 20, stop = ω_range_max, step = 0.01 * omega_p)
+    x2 = range(omega_p / 20, stop = ω_range_max, length = 100)
     # plot(x, ε(x, 1.0))
     # print(Reflectivity.(ε.(x, 1.0)))
     # print(Reflectivity.(ε_damped.(x2)))
 
-    σ = 3.6e5
 
     function reflectivity_nodecay(ω::Float64, ω_p::Float64)
         if ω < ω_p
@@ -34,12 +34,19 @@ function PlotReflectivity()
         end
     end
 
-    # Plots.display(plot(x, Reflectivity.(ε.(x, 1.0)), label = "Reflectivity, σ = ∞ (no decay)"))
+    reflectivity_largedecay(ω) = (ω^2 * ε_0^2 / σ^2) * (√complex(1 + σ / (im * ω * ε_0)) - 1)^2 * (√complex(1 - σ / (im * ω * ε_0)) - 1)^2
+
+    Plots.display(plot(x, Reflectivity.(ε.(x, 1.3)), label = "Reflectivity, σ = ∞ (no decay)", ylims = (0, 1.5)))
     # Plots.display(plot!(x, reflectivity_nodecay.(x, 1.0), label = "Reflectivity, σ = ∞ (no decay); after simplified"))
-    # Plots.display(plot(x2, Reflectivity.(ε_damped.(x2))))
-    Plots.display(plot(x2, Reflectivity.(ε_actual.(x2, omega_p, σ))))
+    Plots.display(plot!(x2 ./ omega_p, Reflectivity.(ε_damped.(x2)), xlabel = "ω/ω_p",
+    label = "Reflectivity, σ → 0 (large decay)"))
+    # Plots.display(plot!(x2 ./ omega_p, reflectivity_largedecay.(x2), label = "Reflectivity, σ → 0 (large decay); after simplified"))
+
+    Plots.display(plot!(x2 ./ omega_p, Reflectivity.(ε_actual.(x2, omega_p, σ)), label = "Reflectivity, theoretical (without approximation)"))
     gui()
     readline()
+    savefig("Reflectivity-of-Free-Electron.svg")
+    savefig("Reflectivity-of-Free-Electron.png")
 end
 
 PlotReflectivity()
